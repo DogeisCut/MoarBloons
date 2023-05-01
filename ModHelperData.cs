9be0c372220ac662cc51fe400c22caf5d2771cb0
DogeisCut <rickisparks06@gmail.com>
2023-05-01 06:27:59 -0600

    Update Info

--- ModHelperData.cs
+++ ModHelperData.cs
@@ -6,8 +6,8 @@ public static class ModHelperData
     public const string Version = "1.0.0";
     public const string Name = "MoarBloons";
 
-    public const string Description = "An empty mod";
+    public const string Description = "Adds a vareity of new bloons, including a custom modeled moab class bloon. Also adds custom rounds and a gamemode to play with these new bloons!";
 
-    public const string RepoOwner = ""; // TODO add your github username hero, also in the download url in README.md
-    public const string RepoName = ""; // TODO add your repo name here, also in the download url in README.md
+    public const string RepoOwner = "DogeisCut"; // TODO add your github username hero, also in the download url in README.md
+    public const string RepoName = "MoarBloons"; // TODO add your repo name here, also in the download url in README.md
 }
